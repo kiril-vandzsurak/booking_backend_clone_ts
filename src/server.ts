305eadaadcb6ac0e2d4fe96da47742f79c3977ec
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-17 15:11:47 +0100

    connecting all stuff to server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,25 @@
 import express from "express";
 import cors from "cors";
+import usersRouter from "./api/user";
+import {
+  badRequestHandler,
+  unauthorizedErrorHandler,
+  forbiddenErrorHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} from "./errorHandlers";
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+
+server.use("/users", usersRouter);
+
+server.use(badRequestHandler);
+server.use(unauthorizedErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
+
 export { server };
